12927f2b4358c7aed5e109da4405375487628963
tharyck <thaka.gus@gmail.com>
2024-06-18 23:16:02 -0300

    feat: csrt

--- studies/vision/src/main.rs
+++ studies/vision/src/main.rs
@@ -6,14 +6,16 @@ use ndarray::{ s, Array2, Array3, ArrayView, ArrayViewMut, Axis };
 use std::{ env, fs, path::PathBuf };
 
 use opencv::{
-    core::{ FileNode, FileStorage, Point, Scalar, Size, CV_8UC1 },
+    core::{ FileNode, FileStorage, Point, Rect, Scalar, Size, CV_8UC1 },
     face::LBPHFaceRecognizer,
     highgui,
     imgcodecs,
     imgproc::{ self, resize },
     objdetect::CascadeClassifier,
     prelude::*,
+    tracking,
     types::{ self, VectorOfMat, VectorOfi32 },
+    videoio,
     Error,
     Result,
 };
@@ -144,11 +146,7 @@ fn train_face_yale_hist() -> Result<(), Error> {
     Ok(())
 }
 
-fn main() -> opencv::Result<()> {
-    // using_haarcascade();
-    // train face
-    // train_face_yale_hist();
-
+fn test_yale_hist() -> opencv::Result<()> {
     let manifest_dir = env::var("CARGO_MANIFEST_DIR").unwrap();
     let project_dir = PathBuf::from(manifest_dir);
 
@@ -216,5 +214,181 @@ fn main() -> opencv::Result<()> {
             break;
         }
     }
+    Ok(())
+}
+
+fn static_csrt() -> opencv::Result<()> {
+    let manifest_dir = env::var("CARGO_MANIFEST_DIR").unwrap();
+    let project_dir = PathBuf::from(manifest_dir);
+
+    let classifier_path = project_dir.clone().join("./city.mp4");
+    let mut video = videoio::VideoCapture::from_file(
+        classifier_path.to_str().unwrap(),
+        videoio::CAP_ANY
+    )?;
+    if !video.is_opened()? {
+        panic!("Failed to open the video file.");
+    }
+
+    // Lê o primeiro frame do vídeo
+    let mut frame = Mat::default();
+    video.read(&mut frame)?;
+
+    // Permite ao usuário selecionar a ROI manualmente
+    let mut roi = highgui::select_roi(&frame, false, false)?;
+    highgui::wait_key(0)?;
+
+    // Verifica se a ROI foi selecionada
+    if roi.width > 0 && roi.height > 0 {
+        // Cria o rastreador CSRT
+        let mut tracker = tracking::TrackerCSRT::create_def()?;
+
+        // Inicializa o rastreador com o primeiro frame e a ROI
+        tracker.init(&frame, roi)?;
+
+        // Loop para ler frames do vídeo e rastrear o objeto
+        while video.read(&mut frame)? {
+            // Atualiza o rastreador com o novo frame
+            let ok = tracker.update(&frame, &mut roi)?;
+
+            if ok {
+                // Se o rastreamento foi bem-sucedido, desenha a nova ROI
+                imgproc::rectangle(
+                    &mut frame,
+                    roi,
+                    Scalar::new(0.0, 255.0, 0.0, 0.0),
+                    2,
+                    imgproc::LINE_8,
+                    0
+                )?;
+            } else {
+                // Se o rastreamento falhou, exibe uma mensagem
+                imgproc::put_text(
+                    &mut frame,
+                    "Tracking failure detected",
+                    Point::new(100, 80),
+                    imgproc::FONT_HERSHEY_SIMPLEX,
+                    0.75,
+                    Scalar::new(0.0, 0.0, 255.0, 0.0),
+                    2,
+                    imgproc::LINE_8,
+                    false
+                )?;
+            }
+
+            // Exibe o frame com a ROI desenhada ou mensagem de falha
+            highgui::imshow("Tracking", &frame)?;
+
+            // Sai do loop se a tecla 'q' for pressionada
+            let key = highgui::wait_key(30)?;
+            if key == ('q' as i32) {
+                break;
+            }
+        }
+    } else {
+        println!("Nenhuma ROI selecionada.");
+    }
+    Ok(())
+}
+
+fn detect_object(frame: &Mat) -> Result<Rect, Error> {
+    // Converter o frame para escala de cinza
+    let mut gray = Mat::default();
+    imgproc::cvt_color(frame, &mut gray, imgproc::COLOR_BGR2GRAY, 0)?;
+
+    // Aplicar um desfoque gaussiano para reduzir o ruído
+    let mut blurred = Mat::default();
+    imgproc::gaussian_blur(&gray, &mut blurred, Size::new(21, 21), 0.0, 0.0, 1)?;
+
+    let mut thresh = Mat::default();
+    imgproc::threshold(&blurred, &mut thresh, 200.0, 255.0, imgproc::THRESH_BINARY)?;
+
+    // Encontrar contornos no frame binarizado
+    let mut contours = opencv::types::VectorOfVectorOfPoint::new();
+    imgproc::find_contours(
+        &thresh,
+        &mut contours,
+        imgproc::RETR_EXTERNAL,
+        imgproc::CHAIN_APPROX_SIMPLE,
+        Point::new(0, 0)
+    )?;
+
+    // Se encontrar contornos, retorna o bounding box do maior contorno
+    if contours.len() > 0 {
+        let largest_contour = contours.get(0).unwrap();
+        let bounding_rect = imgproc::bounding_rect(&largest_contour)?;
+        return Ok(bounding_rect);
+    }
+
+    Err(Error::new(1, ""))
+}
+
+fn main() -> opencv::Result<()> {
+    // using_haarcascade();
+    // train face
+    // train_face_yale_hist();
+    // test_yale_hist();
+
+    let manifest_dir = env::var("CARGO_MANIFEST_DIR").unwrap();
+    let project_dir = PathBuf::from(manifest_dir);
+
+    let classifier_path = project_dir.clone().join("./city.mp4");
+    let mut video = videoio::VideoCapture::from_file(
+        classifier_path.to_str().unwrap(),
+        videoio::CAP_ANY
+    )?;
+    if !video.is_opened()? {
+        panic!("Failed to open the video file.");
+    }
+
+    // Cria uma janela para exibir os frames
+    highgui::named_window("Tracking", highgui::WINDOW_AUTOSIZE)?;
+
+    // Variáveis para rastreamento
+    let mut tracker = tracking::TrackerCSRT::create_def()?;
+    let mut tracking = false;
+    let mut roi = Rect::default();
+
+    // Loop para ler frames do vídeo e rastrear o objeto
+    let mut frame = Mat::default();
+    while video.read(&mut frame)? {
+        if tracking {
+            // Atualiza o rastreador com o novo frame
+            let ok = tracker.update(&frame, &mut roi)?;
+            if ok {
+                //  desenha a nova ROI
+                imgproc::rectangle(
+                    &mut frame,
+                    roi,
+                    Scalar::new(0.0, 255.0, 0.0, 0.0),
+                    2,
+                    imgproc::LINE_8,
+                    0
+                )?;
+            } else {
+                //  desativa o rastreamento
+                tracking = false;
+            }
+        } else {
+            // Detecta o objeto no frame atual
+            if let Ok(new_roi) = detect_object(&frame) {
+                // Inicializa o rastreador com o frame atual e a ROI detectada
+                tracker = tracking::TrackerCSRT::create_def()?;
+                tracker.init(&frame, new_roi)?;
+                roi = new_roi;
+                tracking = true;
+            }
+        }
+
+        // Exibe o frame com a ROI desenhada ou mensagem de falha
+        highgui::imshow("Tracking", &frame)?;
+
+        // Sai do loop se a tecla 'q' for pressionada
+        let key = highgui::wait_key(30)?;
+        if key == ('q' as i32) {
+            break;
+        }
+    }
+
     Ok(())
 }
