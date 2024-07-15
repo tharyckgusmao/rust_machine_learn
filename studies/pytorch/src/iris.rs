255fde5a6dc3e8df2b859e184811b41f3279a341
tharyck <thaka.gus@gmail.com>
2024-07-14 21:09:44 -0300

    feat: adjust types

--- studies/pytorch/src/iris.rs
+++ studies/pytorch/src/iris.rs
@@ -112,7 +112,7 @@ pub fn iris_train() -> Result<(), Box<dyn Error>> {
             col("class")
                 .cast(DataType::Categorical(None, CategoricalOrdering::Physical))
                 .to_physical()
-                .cast(DataType::Float64),
+                .cast(DataType::Int64),
         ])
         .drop_nulls(None)
         .collect()?;
@@ -124,76 +124,73 @@ pub fn iris_train() -> Result<(), Box<dyn Error>> {
 
     println!("targets_vec {:?}", targets_vec);
 
-    let outputs_vec: Vec<f64> = outputs
+    let outputs_vec: Vec<i64> = outputs
         .column("class")?
-        .f64()?
+        .i64()?
         .into_iter()
         .filter_map(|opt_str| opt_str)
         .collect();
 
     println!("outputs_vec {:?}", outputs_vec);
 
+    // Converter para tensores;
+
     let flattened_data = Array::from_iter(targets_vec.clone().iter().cloned());
     let dim = &targets_vec.dim();
 
-    let inputs = Tensor::from_slice(flattened_data.as_slice().unwrap())
+    let inputs_tensor = Tensor::from_slice(flattened_data.as_slice().unwrap())
         .reshape(&[dim.0 as i64, dim.1 as i64])
         .to_kind(Kind::Float);
-    println!("inputs {:?}", inputs);
+    println!("inputs {:?}", inputs_tensor);
 
-    // Converter para tensores
-    // let inputs_tensor = Tensor::stack(&inputs, 0);
-    // let outputs_tensor = Tensor::stack(&outputs, 0);
+    let outputs_tensor = Tensor::from_slice(&outputs_vec).reshape(&[-1]).to_kind(Kind::Int64);
+    println!("outputs {:?}", outputs_tensor);
 
-    // let inputs_tensor = inputs_tensor.to_kind(tch::Kind::Float);
-    // let outputs_tensor = outputs_tensor.to_kind(tch::Kind::Float);
+    let inputs_tensor = inputs_tensor.to_kind(tch::Kind::Float);
+    let outputs_tensor = outputs_tensor.to_kind(tch::Kind::Int64);
 
-    // let num_samples = inputs_tensor.size()[0];
-    // let num_train = ((num_samples as f64) * 0.8) as i64;
-    // let train_inputs = inputs_tensor.narrow(0, 0, num_train);
-    // let test_inputs = inputs_tensor.narrow(0, num_train, num_samples - num_train);
+    let num_samples = inputs_tensor.size()[0];
+    let num_train = ((num_samples as f64) * 0.8) as i64;
+    let train_inputs = inputs_tensor.narrow(0, 0, num_train);
+    let test_inputs = inputs_tensor.narrow(0, num_train, num_samples - num_train);
 
-    // let train_outputs = outputs_tensor.narrow(0, 0, num_train);
-    // let test_outputs = outputs_tensor.narrow(0, num_train, num_samples - num_train);
+    let train_outputs = outputs_tensor.narrow(0, 0, num_train);
+    let test_outputs = outputs_tensor.narrow(0, num_train, num_samples - num_train);
 
-    // // Definir modelo, otimizador e outros parâmetros de treinamento
-    // let vs = VarStore::new(tch::Device::cuda_if_available());
-    // let model = build_model(&vs.root());
+    // Definir modelo, otimizador e outros parâmetros de treinamento
+    let vs = VarStore::new(tch::Device::cuda_if_available());
+    let model = build_model(&vs.root());
 
-    // let config = Adam { beta1: 0.9, beta2: 0.999, wd: 0.0001, eps: 1e-8, amsgrad: false };
+    let config = Adam { beta1: 0.9, beta2: 0.999, wd: 0.0001, eps: 1e-8, amsgrad: false };
 
-    // let mut optimizer = config.build(&vs, 1e-3)?;
+    let mut optimizer = config.build(&vs, 1e-3)?;
 
-    // // Treinamento do modelo
-    // let num_batches = (train_inputs.size()[0] as usize) / BATCH_SIZE;
-    // let epochs = 2000;
-    // for epoch in 1..=epochs {
-    //     let mut epoch_loss = 0.0;
-    //     for batch_idx in 0..num_batches {
-    //         let start_idx = batch_idx * BATCH_SIZE;
-    //         let end_idx = (batch_idx + 1) * BATCH_SIZE;
+    // Treinamento do modelo
+    let num_batches = (train_inputs.size()[0] as usize) / BATCH_SIZE;
+    let epochs = 2000;
+    for epoch in 1..=epochs {
+        let mut epoch_loss = 0.0;
+        for batch_idx in 0..num_batches {
+            let start_idx = batch_idx * BATCH_SIZE;
+            let end_idx = (batch_idx + 1) * BATCH_SIZE;
 
-    //         let inputs_batch = train_inputs.narrow(0, start_idx as i64, BATCH_SIZE as i64);
-    //         let outputs_batch = train_outputs.narrow(0, start_idx as i64, BATCH_SIZE as i64);
+            let inputs_batch = train_inputs.narrow(0, start_idx as i64, BATCH_SIZE as i64);
+            let outputs_batch = train_outputs.narrow(0, start_idx as i64, BATCH_SIZE as i64);
 
-    //         optimizer.zero_grad();
+            optimizer.zero_grad();
 
-    //         let predicted = model.forward_t(&inputs_batch, true);
-    //         // let loss = predicted.copy().mse_loss(&outputs_batch, tch::Reduction::Mean);
-    //         let loss = predicted.binary_cross_entropy::<Tensor>(
-    //             &outputs_batch,
-    //             None,
-    //             Reduction::Mean
-    //         );
-    //         loss.backward();
-    //         optimizer.step();
-    //         epoch_loss += loss.double_value(&[]);
-    //     }
+            let predicted = model.forward_t(&inputs_batch, true);
+            let loss = predicted.copy().cross_entropy_for_logits(&outputs_batch);
 
-    //     if epoch % 10 == 0 {
-    //         println!("Epoch: {}, Loss: {:.4}", epoch, epoch_loss / (num_batches as f64));
-    //     }
-    // }
+            loss.backward();
+            optimizer.step();
+            epoch_loss += loss.double_value(&[]);
+        }
+
+        if epoch % 10 == 0 {
+            println!("Epoch: {}, Loss: {:.4}", epoch, epoch_loss / (num_batches as f64));
+        }
+    }
 
     // let test_accuracy = evaluate_model(&model, &inputs_tensor, &outputs_tensor)?;
     // println!("Test Accuracy: {:.2}%", test_accuracy);
@@ -225,13 +222,11 @@ fn evaluate_model(
 
 fn build_model(vs: &nn::Path) -> impl ModuleT {
     nn::seq()
-        .add(nn::linear(vs / "fc1", 30, 16, Default::default()))
+        .add(nn::linear(vs / "fc1", 4, 4, Default::default()))
         .add_fn(|xs: &Tensor| xs.relu())
         // .add_fn(|xs: &Tensor| xs.dropout(0.1, true))
-        .add(nn::linear(vs / "fc2", 16, 16, Default::default()))
+        .add(nn::linear(vs / "fc2", 4, 4, Default::default()))
         .add_fn(|xs: &Tensor| xs.relu())
         // .add_fn(|xs: &Tensor| xs.dropout(0.1, true))
-        .add(nn::linear(vs / "fc3", 16, 1, Default::default()))
-
-        .add_fn(|xs| xs.sigmoid())
+        .add(nn::linear(vs / "fc3", 4, 3, Default::default()))
 }
